d2f072f24a8451cdb07649fec43f6aa4185874e7
FortyClock <parkhom1358@yandex.ru>
2025-02-25 16:02:00 +0700

    алгортмы для телеметрии

--- server/sutils/movementSutils.cc
+++ server/sutils/movementSutils.cc
@@ -40,8 +40,47 @@ namespace movementSutils{
 
     void moving(int x, int y, Json::Value& mehConfig){
 
+        // текущая позиция робота    
+        int xPosition = mehConfig["intelligence_inf"]["position"]["x"].asInt();
+        int yPosition = mehConfig["intelligence_inf"]["position"]["y"].asInt();
+
+        if (mehConfig["robot_state"]["left_leg"]["capacity"].asInt() <= 20)
+            throw std::logic_error("low left leg capicity");
+
+        if (mehConfig["robot_state"]["right_leg"]["capacity"].asInt() <= 20)
+            throw std::logic_error("low right leg capicity");
+
+        if (mehConfig["robot_state"]["torso"]["capacity"].asInt() <= 20)
+            throw std::logic_error("low torso capicity");
+
+
+        std::string facedTo = mehConfig["intelligence_inf"]["faced_to"].asString();
+
+
+        if (xPosition == x && yPosition == y) { // движение прямо или назад
+            mehConfig["robot_state"]["left_leg"]["capacity"] = mehConfig["robot_state"]["left_leg"]["capacity"].asInt() - 5;
+            mehConfig["robot_state"]["right_leg"]["capacity"] = mehConfig["robot_state"]["right_leg"]["capacity"].asInt() - 5;
+        } else {
+            if (facedTo == "N" || facedTo == "S") {
+                if ((facedTo == "N" && xPosition > x) || (facedTo == "S" && x > xPosition)) {
+                    mehConfig["robot_state"]["left_leg"]["capacity"] = mehConfig["robot_state"]["left_leg"]["capacity"].asInt() - 10;
+                } else if ((facedTo == "N" && x < xPosition) || (facedTo == "S" && xPosition < x)) {
+                    mehConfig["robot_state"]["right_leg"]["capacity"] = mehConfig["robot_state"]["right_leg"]["capacity"].asInt() - 10;
+                }
+            } else if (facedTo == "W" || facedTo == "E") {
+                if ((facedTo == "W" && y > yPosition) || (facedTo == "E" && yPosition > y)) {
+                    mehConfig["robot_state"]["left_leg"]["capacity"] = mehConfig["robot_state"]["left_leg"]["capacity"].asInt() - 10;
+                } else if ((facedTo == "W" && yPosition > y) || (facedTo == "E" && y > yPosition)) {
+                    mehConfig["robot_state"]["right_leg"]["capacity"] = mehConfig["robot_state"]["right_leg"]["capacity"].asInt() - 10;
+                }
+            }
+        }
+        // Уменьшение прочности торса при любом движении
+        mehConfig["robot_state"]["torso"]["capacity"] = mehConfig["robot_state"]["torso"]["capacity"].asInt() - 1;
+    
         mehConfig["intelligence_inf"]["position"]["x"] = x;
         mehConfig["intelligence_inf"]["position"]["y"] = y;
+
     }
 
     void turning(std::string turn, Json::Value& mehConfig){
